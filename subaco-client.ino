42397243e251ce60699542e0c111141727634b69
kn1cht <ito.turukasiwa@gmail.com>
2017-12-26 18:59:35 +0900

    add usb host code

--- subaco-client.ino
+++ subaco-client.ino
@@ -7,27 +7,28 @@
 /=================================*/
 
 #include <ArduinoJson.h>
-#include <HTTPClient.h>
-#include <WiFi.h>
-#include <WiFiClientSecure.h>
-#include <WiFiMulti.h>
 #include <esp_deep_sleep.h>
 #include <esp_sleep.h>
 #include <esp32-hal-log.h>
+#include <HTTPClient.h>
+#include <SPI.h>
+#include <usbhub.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <WiFiMulti.h>
 
 #include "ACS712_05B.hpp"
 #include "my_config.h"
 
 #define JST 3600 * 9
-#define RESET 21
 #define SW_1 22
 #define SW_2 23
 #define ACS712_VIOUT 34
 #define ACS712_OFFST 35
 
 WiFiMulti wifiMulti;
-HardwareSerial USBHost(2);
-ACS712 acs712(ACS712_VIOUT, ACS712_OFFST);   // ACS712 current sensor
+ACS712    acs712(ACS712_VIOUT, ACS712_OFFST);   // ACS712 current sensor
+USB       Usb;
 
 const char* ntp_server1 = "ntp.nict.jp";
 const char* ntp_server2 = "ntp.jst.mfeed.ad.jp";
@@ -37,15 +38,13 @@ String serialNum;
 
 void setup() {
   Serial.begin(115200);
-  USBHost.begin(115200);
   delay(10); // wait for serial monitor
-  pinMode(RESET, OUTPUT);
   pinMode(SW_1, OUTPUT);
   pinMode(SW_2, OUTPUT);
-  digitalWrite(RESET, HIGH);
   digitalWrite(SW_2, HIGH);
   log_i("Hello, this is ESP32.");
   addMyAP(wifiMulti); // my_config.h
+  if (Usb.Init() == -1) { log_e("OSC did not start."); }
 }
 
 void loop() {
@@ -57,26 +56,9 @@ void loop() {
       digitalWrite(SW_2, LOW);
       delay(5);
       digitalWrite(SW_1, HIGH);
-      String usb_descr[3];
-      while(usb_descr[1].length() == 0 || usb_descr[2].length() == 0) {
-        while(USBHost.available() <= 0) { delay(50); }
-        while(USBHost.available() > 0) {
-          String str = USBHost.readStringUntil('\n');
-          log_d("Received str: %s", str.c_str());
-          if(str.startsWith("0")) { usb_descr[0] = str; }
-          else if(str.startsWith("1")) { usb_descr[1] = str; }
-          else if(str.startsWith("2")) { usb_descr[2] = str; }
-        }
-        digitalWrite(RESET, LOW);
-        delay(5);
-        digitalWrite(RESET, HIGH);
-        delay(1000);
-      }
-      while(USBHost.available()) { USBHost.read(); }
-
+      while(Usb.getUsbTaskState() != USB_STATE_RUNNING) { Usb.Task(); }
+      Usb.ForEachUsbDevice(&getStrDescriptors);
       digitalWrite(SW_1, LOW); // usb host shield is off
-      product = usb_descr[1].substring(2);
-      serialNum = usb_descr[2].substring(2);
     }
     log_i("Product Name: %s", product.c_str());
     log_i("Serial Number: %s", serialNum.c_str());
@@ -98,21 +80,16 @@ void loop() {
 }
 
 String getJSON(float current, time_t time_now, String to_dev) {
-    StaticJsonBuffer<500> jsonBuffer;
-    JsonObject& root = jsonBuffer.createObject();
-    root["app"] = SUBACO_APP_ID;
-    JsonObject& record = root.createNestedObject("record");
-    JsonObject& datetime = root.createNestedObject("datetime");
-    JsonObject& currentA = record.createNestedObject("currentA");
-    JsonObject& from_device = record.createNestedObject("from_device");
-    JsonObject& to_device = record.createNestedObject("to_device");
-    currentA["value"] = current;
-    from_device["value"] = "";
-    to_device["value"] = to_dev;
-    datetime["value"] = time_now;
-    String res;
-    root.printTo(res);
-    return res;
+  StaticJsonBuffer<500> jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  root["app"] = SUBACO_APP_ID;
+  root["current"] = current;
+  root["from_device"] = "";
+  root["to_device"] = to_dev;
+  root["datetime"] = time_now;
+  String res;
+  root.printTo(res);
+  return res;
 }
 
 void connectToWiFi() {
@@ -127,8 +104,7 @@ void connectToWiFi() {
 }
 
 void postToServer(String jsonStr) {
-  WiFiClientSecure client;
-  client.setCACert(ca_cert);
+  WiFiClient client;
 
   if (!client.connect(SUBACO_HOST, 443))
     log_e("Connection failed!");
@@ -137,7 +113,6 @@ void postToServer(String jsonStr) {
     String req;
     req = "POST " + String(SUBACO_TAG) + " HTTP/1.1\r\n";
     req += "Content-Type: application/json\r\n";
-    req += "X-Cybozu-API-Token: " + String(SUBACO_API_TOKEN) + "\r\n";
     req += "Host: " + String(SUBACO_HOST) + "\r\n";
     req += "Content-Length: " + String(jsonStr.length()) + "\r\n\r\n";
     req += jsonStr + "\0";
@@ -157,3 +132,48 @@ void postToServer(String jsonStr) {
 
 //esp_sleep_enable_timer_wakeup(uint64_t time_in_us);
 //esp_light_sleep_start();
+
+void getStrDescriptors(UsbDevice *pdev) {
+  uint8_t addr = pdev->address.devAddress;
+  Usb.Task();
+  if (Usb.getUsbTaskState() < USB_STATE_CONFIGURING) { return; } // state must be configuring or higher
+  USB_DEVICE_DESCRIPTOR buf;
+  if (Usb.getDevDescr(addr, 0, DEV_DESCR_LEN, (uint8_t *)&buf) != NULL) { return; }
+  if (buf.iManufacturer > 0) {
+    String manufacturer = getstrdescr(addr, buf.iManufacturer);
+  }
+  if (buf.iProduct > 0) {
+    product = getstrdescr(addr, buf.iProduct);
+  }
+  if (buf.iSerialNumber > 0) {
+    serialNum = getstrdescr(addr, buf.iSerialNumber);
+  }
+}
+
+String getstrdescr(uint8_t addr, uint8_t idx) {
+  uint8_t buf[256], rcode;
+  if (rcode = Usb.getStrDescr(addr, 0, 1, 0, 0, buf) != NULL) { //get language table length
+    log_e("Error retrieving LangID table length: %#08x", rcode);
+    return "";
+  }
+  uint8_t length = buf[0]; //length is the first byte
+  if (rcode = Usb.getStrDescr(addr, 0, length, 0, 0, buf) != NULL) { //get language table
+    log_e("Error retrieving LangID table: %#08x", rcode);
+    return "";
+  }
+  uint8_t langid = (buf[3] << 8) | buf[2];
+  if (rcode = Usb.getStrDescr(addr, 0, 1, idx, langid, buf) != NULL) {
+    log_e("Error retrieving string length: %#08x", rcode);
+    return "";
+  }
+  length = buf[0];
+  if (rcode = Usb.getStrDescr(addr, 0, length, idx, langid, buf) != NULL) {
+    log_e("Error retrieving string: %#08x", rcode);
+    return "";
+  }
+  String descString = "";
+  for (uint8_t i = 2; i < length; i += 2) { //string is UTF-16LE encoded
+    descString += (char)buf[i];
+  }
+  return descString;
+}
